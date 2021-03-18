f73ac3a9c60e3d12c26f78a99b673ad94d6a8b97
Jeff Lu <ml2684@cornell.edu>
2021-03-18 03:29:47 -0400

    Initial Commit

---
+++ Nani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nani
+//
+//  Created by Jeff on 2021/3/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
